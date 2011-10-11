153ec74b48239945ec77b226a6f4baacf36af48b
chansen <chansen@cpan.org>
2011-10-11 14:13:16 +0200

    Bumped version

--- lib/Unicode/UTF8.pm
+++ lib/Unicode/UTF8.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 BEGIN {
-    our $VERSION    = '0.01_06';
+    our $VERSION    = '0.01_07';
     our @EXPORT_OK  = qw[ decode_utf8 encode_utf8 ];
     our %EXPORT_TAGS = (
         all => [ @EXPORT_OK ],
